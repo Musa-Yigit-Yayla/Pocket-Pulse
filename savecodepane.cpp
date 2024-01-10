46f5e69aec2dc51b9f07aced7f5d16dbeb37bcce
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-01-10 16:51:47 +0300

    Additional implementation & design choices

--- savecodepane.cpp
+++ savecodepane.cpp
@@ -2,9 +2,20 @@
 
 SavecodePane::SavecodePane(const string username, const string savecode){
     this->btReveal = new QPushButton(QString("reveal"));
+    this->label1.setText(QString("Your privacy is our main concern"));
+
+    string labelHeader2 = username + ", we will provide you a saving code.\nYou may need this code to reset your password.\nPlease ensure that you write it somewhere safe";
+    this->label2.setText(QString::fromStdString(labelHeader2));
+    this->codeLabel.setText(QString::fromStdString(CODE_HIDDEN_TEXT));
+
 
     QObject::connect(this->btReveal, &QPushButton::clicked, this, &SavecodePane::btRevealHandler);
+    QObject::connect(this->btRegenerate, &QPushButton::clicked, this, &SavecodePane::btRegenHandler);
 }
 void SavecodePane::btRevealHandler(){
 
 }
+void SavecodePane::btRegenHandler(){
+    //Regenerate the saving code
+}
+const string SavecodePane::CODE_HIDDEN_TEXT = "******";
--- savecodepane.h
+++ savecodepane.h
@@ -14,12 +14,16 @@ class SavecodePane: public QWidget{
 private:
     QLabel label1;
     QLabel label2;
+    QLabel codeLabel;
     QPushButton* btReveal = nullptr;
+    QPushButton* btRegenerate = nullptr;
     bool isCodeHidden = true;
 public:
     SavecodePane(const string username, const string passcode);
+    static const string CODE_HIDDEN_TEXT;
 public slots:
     void btRevealHandler();
+    void btRegenHandler();
 };
 
 #endif // SAVECODEPANE_H
