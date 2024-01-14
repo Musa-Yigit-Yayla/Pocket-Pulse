35cacc4ef262a548adeae4cc5a2af00b45be3cce
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-01-14 14:10:34 +0300

    Started initiating database integration

--- maincontroller.cpp
+++ maincontroller.cpp
@@ -1,10 +1,19 @@
 #include "maincontroller.h"
 
-MainController::MainController()
-{
+using namespace std;
+MainController::MainController(){
+    //Create and establish the database connection
+    this->db = QSqlDatabase::addDatabase("QMYSQL");
 
 }
-bool MainController::createUser(User* user){
+MainController::~MainController(){
 
+}
+//Given user has to be checked from the caller to ensure that no such user exists
+bool MainController::createUser(User* user){
+    if(user != nullptr){
+        //proceed registering the user
+    }
 }
 const string MainController::DB_NAME = "PocketPulseDB";
+const string MainController::DB_PASSWORD = "123456";
--- maincontroller.h
+++ maincontroller.h
@@ -1,21 +1,28 @@
 #ifndef MAINCONTROLLER_H
 #define MAINCONTROLLER_H
 
-#include "user.h";
+#include "user.h"
 #include <string>
+#include <cstddef>
+#include <QSqlQuery>
+#include <QSqlDatabase>
 
 using namespace std;
 /**
  * Controller class for interacting with the local database
 */
 class MainController{
+private:
+    QSqlDatabase db;
 
 public:
     MainController();
+    ~MainController();
     static bool createUser(User* user);
 
     //static constants
     static const string DB_NAME;; //change the name when the app is deployed and ensure it matches the created db file
+    static const string DB_PASSWORD;
 };
 
 #endif // MAINCONTROLLER_H
