c5e6f3c239c9e8e84ce7a0aa492a2ceb91b62d3f
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-01-23 00:54:28 +0300

    Animation is initiated
    Further adjust the animation and ensure it traverses all states.

--- animatedlabel.cpp
+++ animatedlabel.cpp
@@ -5,13 +5,14 @@ using namespace std;
 AnimatedLabel::AnimatedLabel(QWidget *parent)
     : QWidget{parent}
 {
-    this->pulse = new Pulse(5000); //ensure you pass the parent as the outer class
+    this->pulse = new Pulse(500, this); //ensure you pass the parent as the outer class
+    this->play();
 }
-void AnimatedLabel::play(){
-
+void inline AnimatedLabel::play(){
+    this->pulse->play();
 }
-void AnimatedLabel::stop(){
-
+void inline AnimatedLabel::stop(){
+    this->pulse->stop();
 }
 void AnimatedLabel::paintEvent(QPaintEvent* event){
     QPainter painter(this);
@@ -60,45 +61,60 @@ void AnimatedLabel::paintEvent(QPaintEvent* event){
         int currY = coordinates.at(1);
 
         int incX = 0, incY = 0; //incremental rates
-        int duration = this->pulse->getDurationMillis();
-        int stateDuration;
+        double stateDuration = ANIMATION_DURATION_SEC; //5 seconds is the expected duration
         //state time distribution is as follows: {s0 = 0.3, s1 = 0.1, s2 = 0.2, s3 = 0.1, s4 = 0.3}
         switch(this->pulse->getState()){
         case 0:
-            stateDuration = duration * 0.3;
+            stateDuration = stateDuration * 0.3;
             incX = (pathPoints.at(2) - pathPoints.at(0)) / stateDuration;
             incY = 0; break;
         case 1:
-            stateDuration = duration * 0.1;
+            stateDuration = stateDuration * 0.1;
             incX = (pathPoints.at(4) - pathPoints.at(2)) / stateDuration;
             incY = (pathPoints.at(5) - pathPoints.at(3)) / stateDuration; break;
         case 2:
-            stateDuration = duration *  0.2;
-            incX =(pathPoints.at(6) - pathPoints.at(4));
-            incY = (pathPoints.at(7) - pathPoints.at(5)); break;
+            stateDuration = stateDuration *  0.2;
+            incX = (pathPoints.at(6) - pathPoints.at(4)) / stateDuration;
+            incY = (pathPoints.at(7) - pathPoints.at(5)) / stateDuration; break;
+        case 3:
+            stateDuration = stateDuration * 0.1;
+            incX = (pathPoints.at(8) - pathPoints.at(6)) / stateDuration;
+            incY = (pathPoints.at(9) - pathPoints.at(7)) / stateDuration; break;
+        case 4:
+            stateDuration = stateDuration * 0.3;
+            incX = (pathPoints.at(10) - pathPoints.at(8)) / stateDuration;
+            incY = 0; break;
         }
         this->pulse->setCoordinates(currX + incX, currY + incY);
     }
     painter.end();
 }
 void AnimatedLabel::repaintSlot() {
+    //qDebug() << "Debug: repaintSlot of animated label has been invoked";
     this->update();
 }
 AnimatedLabel::Pulse::Pulse(int durationMillis, QWidget* parent): QObject{parent}{
     this->durationMillis = durationMillis;
+    this->timer = new QTimer(this);
+    this->timer->setInterval(this->durationMillis);
 
     //bind the timer with paintEvent of the outer class
     AnimatedLabel* al = reinterpret_cast<AnimatedLabel*>(this->parent());
-    QObject::connect(&(this->timer), &QTimer::timeout, al, &AnimatedLabel::repaintSlot);
+    QObject::connect(this->timer, &QTimer::timeout, al, &AnimatedLabel::repaintSlot);
+}
+AnimatedLabel::Pulse::~Pulse(){
+    delete this->timer;
 }
 bool AnimatedLabel::Pulse::isActive() const{
     return this->active;
 }
 void AnimatedLabel::Pulse::play(){
     this->active = true;
+    this->timer->start();
 }
 void AnimatedLabel::Pulse::stop(){
     this->active = false;
+    this->timer->stop();
 }
 void AnimatedLabel::Pulse::setDurationMillis(int durationMillis){
     if(durationMillis > 0){
@@ -116,7 +132,35 @@ void AnimatedLabel::Pulse::setCoordinates(int newX, int newY){
     this->currX = newX;
     this->currY = newY;
 
-    //Don't forget to update the state when applicable
+    //update the state when applicable
+    vector<int> vecX;
+
+    for(int i = 0; i < pathPoints.size(); i += 2){
+        vecX.push_back(pathPoints.at(i));
+    }
+    if(this->currX < vecX.at(1)){
+        //state 0
+        this->state = 0;
+    }
+    else if(this->currX < vecX.at(2)){
+        this->state = 1;
+    }
+    else if(this->currX < vecX.at(3)){
+        this->state = 2;
+    }
+    else if(this->currX < vecX.at(4)){
+        this->state = 3;
+    }
+    else if(this->currX < vecX.at(5)){
+        this->state = 4;
+    }
+    else{
+        //restart the animation
+        this->currX = pathPoints.at(0);
+        this->currY = pathPoints.at(1);
+        this->state = 0;
+    }
+    qDebug() << "The new coordinates are {" << this->currX << ", " << this->currY << " and the new state is " << this->state;
 }
 QRect AnimatedLabel::Pulse::getRect() const{
     return QRect(this->currX, this->currY, this->width, this->height);
@@ -127,6 +171,8 @@ void AnimatedLabel::Pulse::setState(int state){
 int AnimatedLabel::Pulse::getState() const{
     return this->state;
 }
+const vector<int> AnimatedLabel::pathPoints = {0, 35, 50, 35, 56, 10, 58, 60, 64, 35, 120, 35};
 const QColor AnimatedLabel::BACKGROUND_BLUE = QColor(6, 59, 135);
 const QColor AnimatedLabel::PATH_WHITE = QColor(242, 240, 230);
 const QColor AnimatedLabel::Pulse::PULSE_ORANGE = QColor(251, 185, 9);
+
--- animatedlabel.h
+++ animatedlabel.h
@@ -23,12 +23,7 @@ using namespace std;
 class AnimatedLabel : public QWidget{
     Q_OBJECT
 private:
-    vector<int> pathPoints = {0, 35, 50, 35, 56, 10, 58, 60, 64, 35, 120, 35}; //coordinates of the path, in the form of {x0, y0, x1, y1, ..., xn - 1, yn - 1}
-public:
-    explicit AnimatedLabel(QWidget *parent = nullptr);
-    void play();
-    void stop();
-    void paintEvent(QPaintEvent*) override;
+    static const vector<int> pathPoints; //coordinates of the path, in the form of {x0, y0, x1, y1, ..., xn - 1, yn - 1}
 
     static const int HEIGHT = 80;
     static const int WIDTH = 120;
@@ -39,8 +34,15 @@ public:
     static const int POLYLINE_OFFSET_X = 15;
     static const int POLYLINE_OFFSET_Y = 24;
     static const int PULSE_WIDTH_SIZE = 4;
+public:
+    explicit AnimatedLabel(QWidget *parent = nullptr);
+    void inline play();
+    void inline stop();
+    void paintEvent(QPaintEvent*) override;
+
     static const QColor BACKGROUND_BLUE;
     static const QColor PATH_WHITE;
+    static const int ANIMATION_DURATION_SEC = 5;
 
     class Pulse: public QObject{
         //use the scope resolution operator twice to implement the methods of the Pulse class
@@ -49,14 +51,14 @@ public:
         int height = 3;
         int durationMillis;
         bool active = false;
-        int currX = 0;
-        int currY = 0;
+        int currX = pathPoints.at(0);
+        int currY = pathPoints.at(1);
         int state = 0; //5 states {0, 1, 2, 3, 4} which determine where we are positioned along the pulse like path
-        QTimer timer;
+        QTimer* timer = nullptr;
 
     public:
         Pulse(int durationMillis, QWidget* parent = nullptr);
-
+        ~Pulse();
         bool isActive() const;
         void play();
         void stop();
