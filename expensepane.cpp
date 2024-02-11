00d773832431346f5b6e8f6ebd250ea63d46357e
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-02-11 15:36:50 +0300

    Further implementation required
    Resolve switching between generic panes by conserving intended layout behaviors.

--- expensepane.cpp
+++ expensepane.cpp
@@ -2,6 +2,7 @@
 #include "user.h"
 #include "maincontroller.h"
 #include <QDate>
+#include <QIntValidator>
 
 using namespace std;
 
@@ -18,11 +19,40 @@ ExpensePane::ExpensePane(User* user, QWidget* parent): AbstractPane{user, parent
     }
     this->dateSelector = new QComboBox(this);
     this->setCombobox();
+    this->btUpdate = new QPushButton("update", this);
+    this->headerLabel = new QLabel("Set your spenditure goals by specifying limits for each category for the selected month of the selected year.", this);
 
+    QFont font = this->headerLabel->font();
+    font.setPointSize(8);
+    this->headerLabel->setFont(font);
 
+    this->setLayoutManagement();
+    QObject::connect(this->btUpdate, &QPushButton::clicked, this, &ExpensePane::btUpdateSlot);
 }
 void ExpensePane::setLayoutManagement(){
+    this->gridLayout = new QGridLayout(this);
+    QLabel* categoryLabel = new QLabel("Category", this);
+    QLabel* amountLabel = new QLabel("Amount ($)", this);
+    categoryLabel->setStyleSheet("text-decoration: underline;");
+    amountLabel->setStyleSheet("text-decoration: underline;");
+    this->gridLayout->addWidget(this->dateSelector, 0, 4);
+    this->gridLayout->addWidget(categoryLabel, 1, 0);
+    this->gridLayout->addWidget(amountLabel, 1, 1);
 
+    int categoryLength =  static_cast<int>(EXPENSE_CATEGORIES::count);
+    //add the category labels and textfields
+    for(int i = 0; i < categoryLength; i++){
+        int rowIndex = i + 2;
+        QLineEdit* currTf = this->textfields.at(i);
+        QIntValidator* validator = new QIntValidator(currTf); //make the lineEdit only accept integer values
+        currTf->setValidator(validator);
+        this->gridLayout->addWidget(this->categoryLabels.at(i), rowIndex, 0);
+        this->gridLayout->addWidget(currTf, rowIndex, 1);
+    }
+    this->gridLayout->addWidget(this->btUpdate, categoryLength + 2, 3);
+    this->gridLayout->addWidget(this->headerLabel, categoryLength + 3, 0, 1, 4);
+    //set the layout of this pane as the gridLayout
+    this->setLayout(this->gridLayout);
 }
 void ExpensePane::setCombobox(){
     MainController mc;
@@ -33,27 +63,19 @@ void ExpensePane::setCombobox(){
     QString dateStr = date.toString("dd-MM-yyyy");
 
     bool userGoalsDefined = mc.monthlyExpenseGoalsExist(this->user->getUserName());
-    if(!userGoalsDefined){
-        int currMonth = stoi(dateStr.toStdString().substr(3, 5));
-        int currYear = stoi(dateStr.toStdString().substr(6));
+    int currMonth = stoi(dateStr.toStdString().substr(3, 5));
+    int currYear = stoi(dateStr.toStdString().substr(6));
 
-        for(int i = 0; i < 12; i++){
-            string currStr = ExpensePane::getMonthString(currMonth) + " " + to_string(currYear);
-            this->dateSelector->addItem(QString::fromStdString(currStr));
+    for(int i = 0; i < 12; i++){
+        string currStr = ExpensePane::getMonthString(currMonth) + " " + to_string(currYear);
+        this->dateSelector->addItem(QString::fromStdString(currStr));
 
-            if(currMonth == 12){
-                currMonth = 1;
-                currYear++;
-            }
-            else{
-                currMonth++;
-            }
+        if(currMonth == 12){
+            currMonth = 1;
+            currYear++;
         }
-    }
-    else{
-        int day = stoi(dateStr.toStdString().substr(0, 2));
-        if(day == 1){
-            //create a new month entry for the corresponding user in addition to the latest 11 months if necessary
+        else{
+            currMonth++;
         }
     }
 
